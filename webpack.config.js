e965a3f45d7b81c617c5298aa95fa120b447aa12
nataliestam <natalielynnstam@gmail.com>
2018-07-06 14:44:09 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, '/client/index.jsx'),
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react'],
+        },
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/public'),
+  },
+};
